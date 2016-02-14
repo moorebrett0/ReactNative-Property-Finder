9a66b6ac5d334da2010baec20799485525511751
Brett <Brett@grayboxpdx.com>
2016-02-13 23:29:10 -0800

    add new styles

--- HelloWorld/PropertyView.js
+++ HelloWorld/PropertyView.js
@@ -8,3 +8,36 @@ var {
   Text,
   Component
 } = React;
+
+var styles = StyleSheet.create({
+  container: {
+    marginTop: 65
+  },
+  heading: {
+    backgroundColor: '#F8F8F8',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#DDDDDD'
+  },
+  image: {
+    width: 400,
+    height: 300
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    margin: 5,
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    margin: 5,
+    color: '#656565'
+  },
+  description: {
+    fontSize: 18,
+    margin: 5,
+    color: '#656565'
+  }
+});
